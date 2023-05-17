20d8eb57c6e201968aff9eb63c3957d218dbe5ec
Haritha982 <114345177+Haritha982@users.noreply.github.com>
2023-05-17 23:49:15 +0530

    basic calc

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:basic_calculator/basic%20calc.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: SimpleCalc(),
+    debugShowCheckedModeBanner: false,
+  ),);
+}
+
+
